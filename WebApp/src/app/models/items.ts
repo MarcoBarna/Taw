9a41d914c802f7d047bb1b7dff879c703e0bf442
MarcoBarna <marco.barna@outlook.com>
2019-07-22 14:04:56 +0200

    angular init

---
+++ WebApp/src/app/models/items.ts
@@ -0,0 +1,6 @@
+export interface items {
+    code: number;
+    name: string;
+    requiredTime: number;
+    price: number;
+}
---
+++ WebApp/src/app/models/users.ts
@@ -0,0 +1,6 @@
+//*  cashier = 1, waiter = 2, cook = 3, bartender = 4
+
+export interface users {
+    username: string;
+    role: number;
+}
