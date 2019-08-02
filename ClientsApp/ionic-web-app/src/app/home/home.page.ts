26b827ec9ebc74a2bda32c51542797aef328bd50
MarcoBarna <marco.barna@outlook.com>
2019-08-02 11:28:40 +0200

    first page

--- ClientsApp/ionic-web-app/src/app/home/home.page.ts
+++ ClientsApp/ionic-web-app/src/app/home/home.page.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { UserHttpService } from '../services/users-http-service';
+// import { UserHttpService } from '../services/users-http-service';
 import { Router } from '@angular/router';
 
 @Component({
@@ -9,28 +9,29 @@ import { Router } from '@angular/router';
 })
 export class HomePage {
   
-  private errmessage = undefined;
-  private role = undefined;
-  private disableButton;
+  // private errmessage = undefined;
+  // private role = undefined;
+  // private disableButton;
 
-  constructor(private us: UserHttpService, private router: Router) { }
+  // constructor(private us: UserHttpService, private router: Router) { }
 
-  // ngOnInit() {
-  //   this.disableButton = false;
+  // // ngOnInit() {
+  // //   this.disableButton = false;
+  // // }
+
+  // login(username: string, password: string){
+  //   // this.disableButton = true;
+  //   this.us.login(username, password).subscribe( (data) => {
+  //     this.role = this.us.getRole();
+  //     this.errmessage = undefined;
+  //     console.log('Login granted: ' + JSON.stringify(data));
+  //     console.log('User service token: ' + this.us.getToken() );
+  //     // this.router.navigate(['/' + this.role.toLowerCase()])
+  //   }, (err) => {
+  //     console.log('Login error: ' + JSON.stringify(err.error.errormessage) );
+  //     // this.errmessage = (err.error) ? err.error.errormessage || err.error.message : err;
+  //     // this.disableButton = false;
+  //   });
+
   // }
-
-  login(username: string, password: string){
-    // this.disableButton = true;
-    this.us.login(username, password).subscribe( (data) => {
-      this.role = this.us.getRole();
-      this.errmessage = undefined;
-      console.log('Login granted: ' + JSON.stringify(data));
-      console.log('User service token: ' + this.us.getToken() );
-      // this.router.navigate(['/' + this.role.toLowerCase()])
-    }, (err) => {
-      console.log('Login error: ' + JSON.stringify(err.error.errormessage) );
-      // this.errmessage = (err.error) ? err.error.errormessage || err.error.message : err;
-      // this.disableButton = false;
-    });
-
 }
