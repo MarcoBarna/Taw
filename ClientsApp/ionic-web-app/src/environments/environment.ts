2ce6fc38aca672919b1036eb83b9cb573eea54ff
MarcoBarna <marco.barna@outlook.com>
2019-09-14 20:36:27 +0200

    prob works!!!

--- ClientsApp/ionic-web-app/src/environments/environment.ts
+++ ClientsApp/ionic-web-app/src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  base_url: 'http://localhost:3000/'
+  base_url: 'https://mighty-headland-86419.herokuapp.com/'
 };
 /*
  * For easier debugging in development mode, you can import the following file
