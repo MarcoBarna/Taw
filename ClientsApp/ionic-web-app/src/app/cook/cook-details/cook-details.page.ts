c1616d548a4985bbf9d3546b1cb845412a9e6daf
MarcoBarna <marco.barna@outlook.com>
2019-10-09 10:56:20 +0200

    Finished Restaurant App

--- ClientsApp/ionic-web-app/src/app/cook/cook-details/cook-details.page.ts
+++ ClientsApp/ionic-web-app/src/app/cook/cook-details/cook-details.page.ts
@@ -1,25 +1,26 @@
-import { Component, OnInit } from '@angular/core';
-import { MenuController } from '@ionic/angular';
-import { ActivatedRoute, Router } from '@angular/router';
-import { TableHttpService } from 'src/app/services/table-http.service';
-import { UserHttpService } from 'src/app/services/user-http.service';
-import { OrderHttpService } from 'src/app/services/order-http.service';
-import { ItemHttpService } from 'src/app/services/item-http.service';
-import { Orders } from 'src/app/models/Orders';
-import { Items } from 'src/app/models/Items';
-import { SocketioService } from 'src/app/services/socketio.service';
-import { ToastrService } from 'ngx-toastr';
+import { Component, OnInit } from "@angular/core";
+import { MenuController } from "@ionic/angular";
+import { ActivatedRoute, Router } from "@angular/router";
+import { TableHttpService } from "src/app/services/table-http.service";
+import { UserHttpService } from "src/app/services/user-http.service";
+import { OrderHttpService } from "src/app/services/order-http.service";
+import { ItemHttpService } from "src/app/services/item-http.service";
+import { Orders } from "src/app/models/Orders";
+import { Items } from "src/app/models/Items";
+import { SocketioService } from "src/app/services/socketio.service";
+import { ToastrService } from "ngx-toastr";
 
 @Component({
-  selector: 'app-cook-details',
-  templateUrl: './cook-details.page.html',
-  styleUrls: ['./cook-details.page.scss']
+  selector: "app-cook-details",
+  templateUrl: "./cook-details.page.html",
+  styleUrls: ["./cook-details.page.scss"]
 })
 export class CookDetailsPage implements OnInit {
-
   orderID;
   loadedOrder: Orders;
   loadedArrayOrder;
+  loadedBavarageState;
+  loadedBeverageList;
   loadedItemsArray = new Array<Items>();
   loadname;
 
@@ -37,12 +38,16 @@ export class CookDetailsPage implements OnInit {
   ) {
     this.menuCtrl.enable(false);
     this.AcRoute();
-    this.socketio.get().on('Cook', () => {
+    this.socketio.get().on("Cook", () => {
       this.AcRoute();
-      console.log('Cook event recived');
+      console.log("Cook event recived");
     });
-    this.socketio.get().on('Order Added', () => {
-      this.toast.info('New order added');
+    this.socketio.get().on("Bartender", () => {
+      this.AcRoute();
+      console.log("Cook event recived");
+    });
+    this.socketio.get().on("Order Added", () => {
+      this.toast.info("New order added");
     });
   }
 
@@ -58,18 +63,18 @@ export class CookDetailsPage implements OnInit {
           console.log(err);
         });
     } else {
-      console.log('Dish is already compleated');
+      console.log("Dish is already compleated");
     }
   }
 
   CheckColorStatus(value: number) {
     switch (value) {
       case 1:
-        return 'warning';
+        return "warning";
       case 2:
-        return 'success';
+        return "success";
       case 0:
-        return 'danger';
+        return "danger";
     }
   }
 
@@ -87,29 +92,34 @@ export class CookDetailsPage implements OnInit {
         .modifyOrderState(this.orderID)
         .toPromise()
         .then(data => {
-          console.log('Order Completed');
-        }).catch(err => {
+          console.log("Order Completed");
+        })
+        .catch(err => {
           console.log(err);
         });
-      this.router.navigate(['cook']);
+      this.router.navigate(["cook"]);
     } else {
-      return console.log('Order not Completed');
+      return console.log("Order not Completed");
     }
   }
 
   AcRoute() {
     this.ActivatedRoute.paramMap.subscribe(paramMap => {
-      if (!paramMap.has('orderID')) {
+      if (!paramMap.has("orderID")) {
         // redirect
         return;
       }
-      this.orderID = paramMap.get('orderID');
+      this.orderID = paramMap.get("orderID");
       console.log(this.orderID);
       this.ord
         .getOrder(parseInt(this.orderID, 10))
         .toPromise()
         .then(order => {
           this.loadedOrder = order;
+          this.loadedBavarageState = false;
+          this.loadedBeverageList = [];
+          this.loadedBavarageState = this.loadedOrder.beverageState;
+          this.loadedBeverageList = this.loadedOrder.beverageList;
           console.log(this.loadedOrder);
           this.loadedArrayOrder = this.loadedOrder.dishList;
           this.itm
@@ -122,9 +132,8 @@ export class CookDetailsPage implements OnInit {
               });
               this.loadedItemsArray = [];
               this.loadedArrayOrder.forEach(element => {
-                console.log(element);
                 this.loadname.forEach(item => {
-                  if (element === item.code){
+                  if (element === item.code) {
                     this.loadedItemsArray.push(item);
                   }
                 });
@@ -132,7 +141,6 @@ export class CookDetailsPage implements OnInit {
               this.loadedItemsArray.sort((it1: Items, it2: Items) => {
                 return it2.requiredTime - it1.requiredTime;
               });
-              console.log(this.loadedItemsArray);
             })
             .catch(err => {
               console.log(err);
@@ -147,10 +155,10 @@ export class CookDetailsPage implements OnInit {
   ngOnInit() {
     if (
       this.us.get_token() === undefined ||
-      this.us.get_token() === '' ||
+      this.us.get_token() === "" ||
       this.us.get_role() !== 3
     ) {
-      console.log('Acces Denided');
+      console.log("Acces Denided");
       this.us.logout();
     }
   }
